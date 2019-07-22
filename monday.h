f27696376f6f9c8a0b4504df527e596d4d75c5df
kayla <kayla.j.handy@gmail.com>
2019-07-22 15:28:52 -0500

    New event request for

---
+++ monday.h
@@ -0,0 +1,11 @@
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "description": "desc",
+  "type": "object",
+  "required": [],
+  "integrations": [
+    "Hadoop",
+    "UA"
+  ],
+  "properties": {}
+}
